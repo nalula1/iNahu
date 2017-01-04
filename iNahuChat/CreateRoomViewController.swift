9200200fe76c74e91465b628492f41ec694c58dc
GDA Student <gdastudent@PratikbhaisiMac.attlocal.net>
2017-01-03 19:19:41 -0500

    storing user files perfectly

--- iNahuChat/CreateRoomViewController.swift
+++ iNahuChat/CreateRoomViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class CreateRoomViewController: UIViewController {
+class CreateRoomViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var choosePhotoBtn: UIButton!
+    @IBOutlet weak var photoImg: UIImageView!
+    @IBOutlet weak var captionLbl: UITextField!
+    
+    
+    var selectedPhoto: UIImage?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +30,23 @@ class CreateRoomViewController: UIViewController {
     }
     @IBAction func CancelDidTapped(_ sender: AnyObject) { dismiss(animated: true, completion: nil)
     }
+    @IBAction func selectPhoto_Didtapped(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = .camera
+        }else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        self.present(imagePicker, animated: true, completion: nil)
     }
-    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        selectedPhoto = info [UIImagePickerControllerEditedImage] as? UIImagePickerControllerDelegate as! UIImage?
+        photoImg.image = selectedPhoto
+        picker.dismiss(animated: true, completion: nil)
+    }
+    }
+
 
   
